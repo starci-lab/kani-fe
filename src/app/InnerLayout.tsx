1821df2de989b94790c416de15b82a1bbe5c1ba3
starci183 <cuongnvtse160875@gmail.com>
2026-02-01 00:45:59 +0700

    Refactor InnerLayout by removing responsive scaling logic and simplifying the component structure. Eliminate unnecessary state management and dynamic styling, allowing for a more straightforward rendering of children components.

--- src/app/InnerLayout.tsx
+++ src/app/InnerLayout.tsx
@@ -12,23 +12,9 @@ import {
 import { SingletonHookProvider } from "@/hooks/singleton"
 import { ReduxProvider } from "@/redux"
 import { ToastProvider } from "@heroui/react"
-import React, { PropsWithChildren, Suspense, useEffect, useState } from "react"
+import React, { PropsWithChildren, Suspense } from "react"
 
 export const InnerLayout = ({ children }: PropsWithChildren) => {
-    const [scale, setScale] = useState(1)
-    useEffect(() => {
-        const check = () => {
-            if (window.innerWidth < 400) {
-                setScale(0.85)
-            } else {
-                setScale(1)
-            }
-        }
-
-        check()
-        window.addEventListener("resize", check)
-        return () => window.removeEventListener("resize", check)
-    }, [])
     return (
         <Suspense fallback={<Fallback />}>
             <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={true} storageKey="kani-theme">
@@ -37,14 +23,7 @@ export const InnerLayout = ({ children }: PropsWithChildren) => {
                         <PrivyProvider>
                             <ReduxProvider>
                                 <SingletonHookProvider>
-                                    <div style={
-                                        { 
-                                            transform: `scale(${scale})`, 
-                                            transformOrigin: "top left",
-                                            width: `${100 / scale}%`,
-                                        }}>
-                                        {children}
-                                    </div>
+                                    {children}
                                     <ModalContainer />
                                     <DrawerContainer />
                                     <ToastProvider />
